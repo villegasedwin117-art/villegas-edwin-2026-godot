5815549c2e6eaec84dddda6f82c13d4b4f86553b
villegasedwin117-art <villegas.edwin117@gmail.com>
2026-01-22 16:24:38 -0800

    First_level
    Created my first level

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	
+	var direction := Input.get_axis("player_move_left", "player_move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
