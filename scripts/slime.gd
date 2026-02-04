5df005448e04b56344022a465352520762523150
villegasedwin117-art <villegas.edwin117@gmail.com>
2026-02-03 17:34:49 -0800

    enemy
    worked on the enemy and fixed some background issues

---
+++ scripts/slime.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+const SPEED : float = 45.0
+var direction : int = -1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
