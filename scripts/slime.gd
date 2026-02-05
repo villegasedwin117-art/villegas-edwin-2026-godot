233f40971301aaddc7ff78cb4ec310f4e7a8aba6
villegasedwin117-art <villegas.edwin117@gmail.com>
2026-02-05 00:00:02 -0800

    enemy update
    fixed the coding portion of the game though still having issue with the movement of the enemy slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -2,6 +2,20 @@ extends Area2D
 
 const SPEED : float = 45.0
 var direction : int = -1
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+
 func _process(delta: float) -> void:
-	pass
+	
+	if ray_cast_left.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = false
+		
+	if ray_cast_right.is_colliding():
+			direction = +1
+			animated_sprite_2d.flip_h = true
+			
+	position.x += SPEED * direction * delta
+			
